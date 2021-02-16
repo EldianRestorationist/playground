f21de5ee46aff9a23514aab9b00090fa18b3a8b5
Eldian Restorationist <eldianrestorationist@gmail.com>
2021-02-15 19:21:39 -0600

    good practices

---
+++ notes/cleanCode.js
@@ -0,0 +1,14 @@
+//Indentation: spaces or tabs, be consistant;
+//Semicolons: not necessary, good practice for debugging;
+//Line Length: > 80 char, break with 2 indents % after operator;
+//Naming: camelCase, variables noun, functions verb
+
+//Clearly expose structure of page //containers w/ ids
+//Write good comments
+//Avoid large functions, break up into smaller
+//Avoid a lot of mixing of coding languages (css inline, js tags)
+//Summarize imports (!too many style sheets)
+
+//Comments say why it works, not how
+//write code simply = less comments
+//Code tells the story, not the comments
